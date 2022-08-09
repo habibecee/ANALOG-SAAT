b4bdac1d7ae33c9b82303f685b203a94ed04efcf
”habibecee” <habibe.ce1996@gmail.com>
2022-08-09 11:42:24 +0300

    readme eklendi

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ANALOG-SAAT
+<h1> PROJE:  JAVA SCRIPT İLE ANALOG SAAT YAPIMI  </h1> </br>
+
+<h2> Proje İçeriği </h2> </br>
+<p> Bu proje JavaScript öğrenimi için yapılmıştır. </br>
+Projede JS kullanılarak analog saat yapımı denenmiştir.  </p> </br> </br>
+
+<h3>  GÖRÜNÜM </h3> </br>
+
+![](analogsaat-screen.gif)
--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function tiktak(){
     let minute = new Date().getMinutes();
     let hour = new Date().getHours(); 
 
-    sec.style.transform = `rotate(${180 + (second * 6)}deg)`
-    min.style.transform = `rotate(${180 + (minute * 6)}deg)`
-    hour.style.transform = `rotate(${180 + (hour * 30)}deg)`
+    sec.style.transform = `rotate(${180 + (second * 6)}deg)`;
+    min.style.transform = `rotate(${180 + (minute * 6)}deg)`;
+    hour.style.transform = `rotate(${180 + (hour * 30)}deg)`;
 
     console.log({second,minute,hour});
 }
